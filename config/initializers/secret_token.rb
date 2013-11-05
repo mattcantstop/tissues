7e1ea4bb2f26ae6f42a383832c2ae0f9cbbc702c
Matt Duff <matthewlduff@gmail.com>
2013-11-05 07:35:48 -0700

    created project with rspec and postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tissues::Application.config.secret_key_base = '92ea804677266ef3c077657a2b8ef7d45e74231abba926299868f3f4f4447349de7392d7b37aebb7e61a6502a30708baba36415851e16d74f623716bf0f4cec2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tissues::Application.config.session_store :cookie_store, key: '_tissues_session'
