67be64db96c28f87f7ed2668201c1a645d03e2f0
Matt Duff <matthewlduff@gmail.com>
2013-11-07 09:09:58 -0700

    capybara test to check for project url, added files

---
+++ spec/features/viewing_projects_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature "Viewing projects" do
+  scenario "listing all projects" do
+    project = FactoryGirl.create(:project, name: "Textmate 2")
+    visit '/'
+    click_link "Textmate 2"
+    expect(page.current_url).to eql(project_url(project))
+  end
+end
