913fcdd20f5ada3b6d278a79618b7c96c42f75d1
Matt Duff <matthewlduff@gmail.com>
2013-11-08 08:14:41 -0700

    added a DRY method for the project controller, rescue for id not found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: [:show,
+                                     :edit,
+                                     :update,
+                                     :destroy]
+
   def index
     @projects = Project.all
   end
@@ -20,15 +25,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -39,7 +41,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     if @project.delete
       flash[:notice] = "Project has been destroyed."
       redirect_to projects_path
@@ -54,4 +55,11 @@ class ProjectsController < ApplicationController
   def project_params
     params.require(:project).permit(:name, :description)
   end
+
+  def set_project
+    @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" + " for can not be found."
+      redirect_to projects_path
+    end
 end
