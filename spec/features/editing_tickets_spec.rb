ff024bfb6e8b6e530c0cdc6ff93cf9ad2b820210
Matthew Duff <matthewlduff@gmail.com>
2013-11-16 08:58:37 -0800

    fixed editing a ticket spec

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 feature "Editing tickets" do
+  let!(:user)    { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project) }
-  let!(:ticket)  { FactoryGirl.create(:ticket, project: project) }
+  let!(:ticket)  { FactoryGirl.create(:ticket, project: project, user: user) }
 
   before do
+    sign_in_as!(user)
     visit '/'
     click_link project.name
     click_link ticket.title
