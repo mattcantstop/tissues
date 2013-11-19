5dfd6feb52bfd37a47c9f0e65f43d3ddbc792aba
Matt Duff <matthewlduff@gmail.com>
2013-11-19 09:01:43 -0700

    added two more tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,25 +1,35 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  let!(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    sign_in(user)
+  end
+
+  it "displays an error for a missing project" do
+    get :show, id: "not-here"
+
+    expect(response).to redirect_to(projects_path)
+    message = "The project you were looking for can not be found."
+
+    expect(flash[:alert]).to eql(message)
+  end
 
   context "standard users" do
-    before do
-      sign_in(user)
-    end
+    { new: :get,
+      create: :post,
+      edit: :get,
+      update: :put,
+      destroy: :delete }.each do |action, method|
 
-    it "cannot access the new action" do
-      get :new
+      it "cannot access the #{action} action" do
+        sign_in(user)
 
-      expect(response).to redirect_to('/')
-      expect(flash[:alert]).to eql("You must be an admin to do that.")
-    end
+        send(method, action, :id => FactoryGirl.create(:project))
 
-    it "displays an error for a missing project" do
-      get :show, id: "not-here"
-      expect(response).to redirect_to(projects_path)
-      message = "The project you were looking for can not be found."
-      expect(flash[:alert]).to eql(message)
+        expect(response).to redirect_to(root_path)
+        expect(flash[:alert]).to eql("You must be an admin to do that.")
+      end
     end
   end
 end
