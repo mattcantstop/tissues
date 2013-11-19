2e779e331a14a6290ec50b6edb037a323dd44f3f
Matt Duff <matthewlduff@gmail.com>
2013-11-18 19:29:30 -0700

    added helpers for signin and auth

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,7 @@
 class ProjectsController < ApplicationController
 
-  before_action :set_project, only: [:show,
-                                     :edit,
-                                     :update,
-                                     :destroy]
+  before_action :authorize_admin!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -62,5 +60,14 @@ class ProjectsController < ApplicationController
     rescue ActiveRecord::RecordNotFound
       flash[:alert] = "The project you were looking" + " for can not be found."
       redirect_to projects_path
+  end
+
+  def authorize_admin!
+    require_signin!
+
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
     end
+  end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -57,16 +57,4 @@ class TicketsController < ApplicationController
     params.require(:ticket).permit(:title, :description, :user_id)
   end
 
-  def require_signin!
-    if current_user.nil?
-      flash[:error] = "You need to sign in or sign up before continuing."
-      redirect_to signin_url
-    end
-  end
-  helper_method :require_signin!
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
 end
