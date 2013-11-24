739e43c5c8790b910a194bcc6bd66d8b744ff719
Matt Duff <matthewlduff@gmail.com>
2013-11-24 11:56:27 -0700

    fixed all failing specs by adding permissions in specs

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -4,15 +4,19 @@ feature 'Deleting tickets' do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:ticket) {
     FactoryGirl.create(:ticket, project: project, user: user) }
+
   before do
+    define_permission!(user, "view", project)
     sign_in_as!(user)
     visit '/'
     click_link project.name
     click_link ticket.title
-end
+  end
+
   scenario "Deleting a ticket" do
     click_link "Delete Ticket"
     expect(page).to have_content("Ticket has been deleted.")
     expect(page.current_url).to eq(project_url(project))
   end
+
 end
