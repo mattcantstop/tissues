902d41873d4a150d46b6ca398292d35ea5ef0e78
Matthew Duff <matthewlduff@gmail.com>
2013-11-28 09:01:56 -0700

    removed some whitespace

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,5 @@
 class TicketsController < ApplicationController
-  
+
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   before_action :require_signin!, except: [:show, :index]
