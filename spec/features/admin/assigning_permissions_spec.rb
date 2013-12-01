8c0d2da88bef1078267f9ca33ba133290f037706
Matthew Duff <matthewlduff@gmail.com>
2013-11-30 21:41:58 -0700

    permissions checkbox page completed and tested

--- spec/features/admin/assigning_permissions_spec.rb
+++ spec/features/admin/assigning_permissions_spec.rb
@@ -24,4 +24,20 @@ feature "Assigning permissions" do
     sign_in_as!(user)
     expect(page).to have_content(project.name)
   end
+
+  scenario "creating tickets for a project" do
+    check_permission_box "view", project
+    check_permission_box "create tickets", project
+    click_button "Update"
+    click_link "Sign Out"
+
+    sign_in_as!(user)
+    click_link project.name
+    click_link "New Ticket"
+    fill_in "Title", with: "Shiny"
+    fill_in "Description", with: "Make it so!"
+    click_button "Create"
+    
+    expect(page).to have_content("Ticket has been created.")
+  end
 end
