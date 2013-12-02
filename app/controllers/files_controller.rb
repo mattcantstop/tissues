f6a8639950016d7c5081c5727de7c4f305cb9b4c
Matt Duff <matthewlduff@gmail.com>
2013-12-02 07:59:28 -0700

    only serving files to authorized users

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,16 @@
 class FilesController < ApplicationController
+
   before_filter :require_signin!
 
-   def show
+  def show
     asset = Asset.find(params[:id])
-    send_file asset.asset.path, filename: asset.asset_identifier,
-                                content_type: asset.content_type
-   end
+    if can?(:view, asset.ticket.project)
+      send_file asset.asset.path, filename: asset.asset_identifier,
+                                  content_type: asset.content_type
+    else
+      flash[:alert] = "The asset you were looking for could not be found."
+      redirect_to root_path
+    end
+  end
 
 end
