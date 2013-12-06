c8c6e77b53adc7861c4a51b8a74a59bb14b1f063
Matt Duff <matthewlduff@gmail.com>
2013-12-06 08:03:55 -0700

    added spec file and wrote spec, still red

---
+++ spec/integration/creating_comments_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+feature "adding comments" do
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:project) { Factory(:project) }
+  let!(:ticket) { Factory(:ticket, project: project, :user => user) }
+
+  before do
+    define_permission!(user)
+
+    sign_in_as!(user)
+    visit '/'
+    click_link project.name
+  end
+
+  scenario "Creating a comment" do
+    click_link ticket.title
+    fill_in "Text", with: "Added a comment!"
+    click_button "Create Comment"
+    page.should have_content("Comment has been created!")
+    within("#comments") do
+      page.should have_content("Added a comment!")
+    end
+  end
+
+  scenario "Creating an invalid comment" do
+    click_link ticket.title
+    click_button "Create Comment"
+    page.should have_content("Comment has not been created.")
+    page.should have_content("Text can't be blank")
+  end
+
+end
