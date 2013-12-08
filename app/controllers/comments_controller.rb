cf7a91b65ed684478d384389b3ed7c0b21177ce5
Matthew Duff <matthewlduff@gmail.com>
2013-12-08 10:17:07 -0700

    added comment params for strong parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,27 @@
 class CommentsController < ApplicationController
 
-   before_filter :authenticate_user!
-   before_filter :find_ticket
+  before_filter :find_ticket
 
-   def create
-     @comment = @ticket.comments.build(params[:comment])
-     @comment.user = current_user
-     if @comment.save
-       flash[:notice] = "Comment has been created."
-       redirect_to [@ticket.project, @ticket]
-     else
-       flash[:alert] = "Comment has not been created."
-       render :template => "tickets/show"
-     end
-   end
+  def create
+    @comment = @ticket.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@ticket.project, @ticket]
+    else
+      flash[:alert] = "Comment has not been created."
+      render :template => "tickets/show"
+    end
+  end
 
-   private
+  private
 
-   def find_ticket
-     @ticket = Ticket.find(params[:ticket_id])
-   end
+  def find_ticket
+    @ticket = Ticket.find(params[:ticket_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :ticket_id)
+  end
 
 end
