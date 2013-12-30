01f78945e9ee2ea7a896c6cea7e14131b039699b
Matthew Duff <matthewlduff@gmail.com>
2013-12-30 10:51:05 -0700

    added spec for changing a tickets state

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -29,4 +29,14 @@ feature "Creating comments" do
     page.should have_content("Text can't be blank")
   end
 
+  scenario "Changing a ticket's state" do
+    fill_in "Text", :with => "This is a real issue"
+    select "Open", :from => "State"
+    click_button "Create Comment"
+    page.should have_content("Comment has been created.")
+    within("#ticket .state") do
+      page.should have_content("Open")
+    end
+  end
+
 end
