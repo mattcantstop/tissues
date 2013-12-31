17c7e2a37e626c8d1d0d7dbc32e6f3029365397b
Matthew Duff <matthewlduff@gmail.com>
2013-12-31 11:25:57 -0700

    added seed file to make form display 389

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+admin_user = User.create(email: "admin@example.com",
+                         name: "admin",
+                         password: "password",
+                         password_confirmation: "password",
+                         admin: true)
+Project.create(name: "Ticketee Beta")
+
+State.create(      name: "New",
+             background: "#85FF00",
+                  color: "white",
+                default: true)
+
+State.create(      name: "Open",
+             background: "#00CFFD",
+                  color: "white")
+
+State.create(      name: "Closed",
+             background: "black",
+                  color: "white")
