f5dd9e4eba0a84bc069b7c338e4d232210fb502d
Matthew Duff <matthewlduff@gmail.com>
2014-01-09 08:36:47 -0700

    added callback on comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,15 @@
 class Comment < ActiveRecord::Base
 
-  validates :text, presence: true
+  after_create :set_ticket_state
 
+  belongs_to :ticket
   belongs_to :user
   belongs_to :state
+
+  validates :text, :presence => true
+
+  def set_ticket_state
+
+  end
 
 end
