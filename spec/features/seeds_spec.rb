1da5f935c77dc8b935634db2f977a5a4cd32b4e7
Matthew Duff <matthewlduff@gmail.com>
2014-01-10 08:56:11 -0700

    test to verify seeds 391

---
+++ spec/features/seeds_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature "Seed Data" do
+  let!(:user) { FactoryGirl.create(:admin_user) }
+
+  scenario "The basics" do
+
+    load Rails.root + "db/seeds.rb"
+    sign_in_as!(user)
+    click_link "Ticketee Beta"
+    click_link "New Ticket"
+    fill_in "Title", :with => "Comments with state"
+    fill_in "Description", :with => "Comments always have a state."
+    click_button "Create Ticket"
+    within("#comment_state_id") do
+      page.should have_content("New")
+      page.should have_content("Open")
+      page.should have_content("Closed")
+    end
+  end
+end
