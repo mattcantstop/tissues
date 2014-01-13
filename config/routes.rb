fc6f7cbabecc588cb1177dd1428696a2d32c19f3
Matthew Duff <matthewlduff@gmail.com>
2014-01-13 08:03:13 -0700

    added make_default route in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Tissues::Application.routes.draw do
 
   namespace :admin do
     root "base#index"
-    resources :states
+    resources :states do
+      member do
+        get :make_default
+      end
+    end
+  end
+
     resources :users do
       resources :permissions
       put "permissions", to: "permissions#set",
