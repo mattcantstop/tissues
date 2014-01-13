99d1b1b35af4211734f0da35037de04a15b93211
Matthew Duff <matthewlduff@gmail.com>
2014-01-13 07:36:06 -0700

    added index action for state

--- app/controllers/admin/states_controller.rb
+++ app/controllers/admin/states_controller.rb
@@ -1,2 +1,21 @@
 class Admin::StatesController < ApplicationController
+
+  def index
+    @states = State.all
+  end
+
+  def new
+    @state = State.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def states_params
+    
+  end
+
 end
