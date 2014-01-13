6b7ba5df0702fed0d22eb790a993bac66d8106ba
Matthew Duff <matthewlduff@gmail.com>
2014-01-13 07:29:23 -0700

    changed tutorials incorrect states creation

--- spec/features/creating_states_spec.rb
+++ spec/features/creating_states_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 feature 'Creating states' do
+  let!(:admin_user) { FactoryGirl.create(:admin_user) }
+
   before do
-    sign_in_as!(Factory(:admin_user))
+    sign_in_as!(admin_user)
   end
 
   scenario "Creating a state" do
