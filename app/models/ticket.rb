b905fad58d6aa77c9c6a22136736fba9337b7f67
Matthew Duff <matthewlduff@gmail.com>
2014-01-16 08:50:32 -0700

    added tags method on tickets and associated them

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,8 @@ class Ticket < ActiveRecord::Base
 
   attr_accessor :tag_names
 
+  before_create :associate_tags
+
   ###
   # Validations
   ###
@@ -20,5 +22,15 @@ class Ticket < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :tags
 
+
+  private
+
+  def associate_tags
+    if tag_names
+      tag_names.split(" ").each do |name|
+        self.tags << Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 
 end
