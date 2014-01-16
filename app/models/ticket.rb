1e96e949d5a4cae56c06137b962e7b9884d5f439
Matthew Duff <matthewlduff@gmail.com>
2014-01-16 08:30:39 -0700

    added view of tags in show and tags to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ActiveRecord::Base
 
-  attr_accessor :tag_names
+  attr_accessible :description, :title, :assets_attributes, :tag_names
 
   ###
   # Validations
@@ -16,6 +16,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :state
   belongs_to :user
   has_many :assets
+  has_many :tags
   accepts_nested_attributes_for :assets
   has_many :comments
 
