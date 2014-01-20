10b0de04fa64afe29f1a59a5a9829237e796838e
Matthew Duff <matthewlduff@gmail.com>
2014-01-19 17:09:42 -0700

    added before create action to associate tag

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ActiveRecord::Base
 
+  attr_accessor :tag_names
+
   before_create :set_previous_state
   after_create :set_ticket_state
+  after_create :associate_tags_with_ticket
 
   belongs_to :ticket
   belongs_to :user
@@ -15,6 +18,16 @@ class Comment < ActiveRecord::Base
 
   def set_previous_state
     self.previous_state = ticket.state
+  end
+
+  def associate_tags_with_ticket
+    if tag_names
+      tags = tag_names.split(" ").map do |name|
+        Tag.find_or_create_by_name(name)
+      end
+      self.ticket.tags += tags
+      self.ticket.save
+    end
   end
 
   def set_ticket_state
