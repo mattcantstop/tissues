c7aed7aff12041580b8e558e7fb1d7fcb0dc77aa
Matthew Duff <matthewlduff@gmail.com>
2014-01-24 08:08:18 -0700

    added route for new search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,11 @@ Tissues::Application.routes.draw do
   delete "/signout", to: "sessions#destroy", as: "signout"
 
   resources :projects do
-    resources :tickets
+    resources :tickets do
+      collection do
+        get :search
+      end
+    end
   end
 
   resources :tickets do
