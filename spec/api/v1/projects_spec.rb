91603fdb408046fc631d5877d39716125d61d69b
Matthew Duff <matthewlduff@gmail.com>
2014-02-02 10:40:06 -0700

    added rspec module for helpers and factory user

---
+++ spec/api/v1/projects_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+  describe "/api/v1/projects", :type => :api do
+    describe "/api/v1/projects", :type => :api do
+      let!(:admin) { Factory(:admin_user) }
+    end
+
+
+end
