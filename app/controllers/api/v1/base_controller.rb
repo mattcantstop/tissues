7ebcc2912cf3caf6b70ec5c9214f972cd7c6b14f
Matt Duff <matt.duff@moneydesktop.com>
2014-02-03 09:36:46 -0700

    base controller authentication method

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,12 +8,16 @@ class Api::V1::BaseController < ActionController::Base
 
   def authenticate_user
     @current_user = User.find_by_authentication_token(params[:token])
-  end
-
-  def authenticate_user
-    @current_user = User.find_by_authentication_token(params[:token])
     unless @current_user
       respond_with({:error => "Token is invalid." })
+    end
+  end
+
+  def authorize_admin!
+    if !@current_user.admin?
+      error = { :error => "You must be an admin to do that." }
+      warden.custom_failure!
+      render params[:format].to_sym => error, :status => 401
     end
   end
 
