1ebe4d03ba875b8714a68b395cb5d60eaa1b8e80
Matt Duff <matt.duff@moneydesktop.com>
2014-02-03 07:56:42 -0700

    added successful create action to projects controller and passing test 545

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::ProjectsController < Api::V1::BaseController
     respond_with(Project.for(current_user).all)
   end
 
+  def create
+    project = Project.new(project_params)
+    if project.save
+      respond_with(project, :location => api_v1_project_path(project))
+    else
+      respond_with(project)
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+
 end
